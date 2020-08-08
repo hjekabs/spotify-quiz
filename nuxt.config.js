2aeb42d7a85a6398c73d73552889434ed32053cc
Jekabs <jekabs@Jekabss-MacBook-Pro.local>
2020-08-08 21:35:53 +0300

    changes /callback

--- nuxt.config.js
+++ nuxt.config.js
@@ -69,7 +69,8 @@ export default {
         response_type: 'token',
         token_type: 'Bearer',
         redirect_uri:
-          `${process.env.BASE_URL}callback` || 'http://localhost:3000/callback',
+          `${process.env.BASE_URL}/callback` ||
+          'http://localhost:3000/callback',
         client_id: 'f79dd1f6402b41fe8828c55654cea80a',
         token_key: 'access_token'
       }
