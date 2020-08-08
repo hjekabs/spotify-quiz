629dd9de3aacf4e1a5e118d4173f83e306d16c3c
Jekabs <jekabs@Jekabss-MacBook-Pro.local>
2020-08-08 21:24:32 +0300

    fixed port

--- io/index.js
+++ io/index.js
@@ -10,7 +10,11 @@ export default function() {
     // overwrite nuxt.server.listen()
     this.nuxt.server.listen = (port, host) =>
       new Promise(resolve =>
-        server.listen(port || 3000, host || 'localhost', resolve)
+        server.listen(
+          process.env.PORT || 3000,
+          process.env.HOST || 'localhost',
+          resolve
+        )
       )
     // close this server on 'close' event
     this.nuxt.hook('close', () => new Promise(server.close))
