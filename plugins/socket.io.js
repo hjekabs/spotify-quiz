29d2c9f45f73b03b7076cf5c7456cdcfc49d8b90
Jekabs <jekabs@Jekabss-MacBook-Pro.local>
2020-08-08 21:49:05 +0300

    changed socket URL

--- plugins/socket.io.js
+++ plugins/socket.io.js
@@ -1,4 +1,4 @@
 import io from 'socket.io-client'
-const socket = io(process.env.BASE_URL || 'http://localhost:3000')
+const socket = io('https://juriz-spotify-quiz.herokuapp.com/')
 
 export default socket
